e2c82eba21ba7adff1eb09e2b65a359b33084bf9
sergiogmoraleda <sergio.garcia52@alu.uclm.es>
2021-10-20 18:35:35 +0200

    adding react-routing and bootstrap buttons

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import {saveAs} from 'file-saver';
+import React from "react";
 import carboneSDK from "carbone-sdk-js";
-import { generateReport } from './generateReport';
-
-const _carboneService = carboneSDK("test_eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI0NzIyIiwiYXVkIjoiY2FyYm9uZSIsImV4cCI6MjI2NDY1NjEwMiwiZGF0YSI6eyJpZEFjY291bnQiOjQ3MjJ9fQ.AOa9Vl0tQwz1yoi1WrzmcMlulgpkZjMpbVpLIE5gG7Kc9brXrX11s4JJWxPFynqqlorv7GmxeTdWy0sqzI37tTilAO-fiq_ZJCpkTbiUrupPZi2wBh8Pkc-HJ1tBwMBEfNExNBMKOtpTMmk8vMu_MlyA3QopfHIs2EMGL6eD_-vYnWMF");
+import { generateReport } from "./generateReport";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
 //const _template = document.getElementById("21bc2ea02373fb14dfe34e490d52812f67b0a197f52668dc9faf7fb547f341a1");
-
- 
-
-
+//onClick={generateReport}
 function App() {
-  return(
-    
-  <>
-  
-    <div class= "container">
-      
-          <button id = "buttonReport" onClick= {
-            
-            generateReport
-            
-            }>
-
-             Generate Report
-            
-          </button>
-    </div>
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <div className="container mt-5">
+              <div className="btn-group">
+                <Button onClick= {generateReport}variant="secondary" size="md">
+                  Generate Report
+                </Button>{" "}
+              </div>
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     </>
-
-  
-  )
+  );
 }
 
 export default App;
